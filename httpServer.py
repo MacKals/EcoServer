7b8f6bf408beae2a20eda6784aa94efbe4e98e11
Morten Kals <morten@kals.no>
2019-05-14 17:51:43 -0700

    untested implementation

--- httpServer.py
+++ httpServer.py
@@ -10,19 +10,10 @@ import mysql.connector
 
 
 class PostHandler(BaseHTTPRequestHandler):
-    
-    # Initialize variables for communicating with data-base
-   
-       
-    #engine = mysql.connector.connect(
-    #        user="ecohydro",
-    #        password="7fQh53G6D2BuCnT54hXH95CJx",
-    #        host="ubc-ecohydro-nodes-mysql-database.cycweraudmq1.us-east-1.rds.amazonaws.com",
-    #        port='3306',
-    #        database='eco_nodes'
-    #)
 
-    #cur = engine.cursor()
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c, d)
+        self.database = EcoDatabase()
 
     def do_POST(self):
         # Parse the form data posted
@@ -34,69 +25,106 @@ class PostHandler(BaseHTTPRequestHandler):
         print('Parsing ', payload)
 
         # authentication
-        
+
 
         # extract node information
-        node = [s for s in str.split() if s.isdigit()]
-        print(node)
-        print(type(node))
-        
+        node_id = str([s for s in nodeString.split() if s.isdigit()])
+        print(node_id)
+        print(type(node_id))
+
 
         if payload[0] == 'C':
             # config message
-            parseTitleString(node, data)
+            parseTitleString(node_id, data)
         else:
             # data message
-            parseDataString(node, data)
+            parseDataString(node_id, data)
 
 
-    def writeDataToDatabase(node, boot_count, sensor, param, read_time, store_time, reading):
-        print(f'call eco_nodes.insert_data({node}, {boot_count}, {sensor}, {param}, \'{read_time}\', \'{store_time}\', {reading});')
-        self.cur.execute(f'call eco_nodes.insert_data({node}, {boot_count}, {sensor}, {param}, \'{read_time}\', \'{store_time}\', {reading});')
-        self.engine.commit()
-
-    def insert_sensor_types():
-        
-
-    def writeConfigToDatabase()
-        (id, type, name, manufacturer, description)
-        
-    def parseDataString(node, string):
+    def parseDataString(self, node_id, string):
         entries = string.split('&')
+
+        boot_count = entries[0]
 
         read_time = datetimeFromUnixTime(entries[1])
         store_time = getCurrentTime()
 
-        boot_count = entries[2]
-
-        for entry in entries[3:]:
+        for entry in entries[2:]:
             e = entry.split(':')
 
-            sensor = e[0]
+            sensor_address = e[0]
             readings = e[1].split(',')
 
-            for param, reading in enumerate(readings):
+            for parameter_number, data_point in enumerate(readings):
                 #writeDataToDatabase(int(node), int(boot_count), int(sensor), param, read_time, store_time, float(reading));
-                writeDataToDatabase(node, boot_count, sensor, param, read_time, store_time, reading);
-    
-    def parseTitleString(node, string):
+                self.database.insert_data_point(node_id, boot_count, sensor_address, parameter_number, read_time, store_time, data_point);
+
+    def parseTitleString(self, node_id, string):
         entries = string[1:].split('&')
-        
-        boot_time = datetimeFromUnixTime(entries[0])
+
+        boot_count = entries[0]
+
+        boot_time = datetimeFromUnixTime(entries[1])
         store_time = getCurrentTime()
 
-        boot_count = entries[1]
+        self.database.insert_node_setup(node_id, boot_count, boot_time, store_time)
 
         for entry in entries[2:]:
-            writeSeria
+            e = entry.split(':')
 
-        print("title string: " + string)
-    
+            sensor_address = e[0]
+            sensor_serial_number = e[1]
+
+            self.database.insert_node_sensor(node_id, boot_count, sensor_address, sensor_serial_number)
+
+
+# wrapper for communicating with database
+class EcoDatabase:
+
+    # Initialize variables for communicating with data-base
+    def __init__(self):
+        self.engine = mysql.connector.connect(
+               user="ecohydro",
+               password="7fQh53G6D2BuCnT54hXH95CJx",
+               host="ubc-ecohydro-nodes-mysql-database.cycweraudmq1.us-east-1.rds.amazonaws.com",
+               port='3306',
+               database='eco_nodes'
+        )
+
+        self.cur = self.engine.cursor()
+
     # Cleanup variables for talking to database
-#    def __del__(self):
- #       print("closed")
-        #cur.close()
-        #engine.close()
+    def __del__(self):
+        print("closed")
+        self.cur.close()
+        self.engine.close()
+
+
+    def insert_data_point(self, node_id, boot_count, sensor_address, parameter_number, read_time, store_time, data_point):
+        query = f'call eco_nodes.insert_data_point({node_id}, {boot_count}, {sensor_address}, {parameter_number}, \'{read_time}\', \'{store_time}\', {data_point});'
+        print(query)
+        self.cur.execute(query)
+        self.engine.commit()
+
+    def insert_node_sensor(self, node_id, boot_count, sensor_address, sensor_serial_number):
+        query = f'call eco_nodes.insert_node_sensor({node_id}, {boot_count}, {sensor_address}, {sensor_serial_number});'
+        print(query)
+        self.cur.execute(query)
+        self.engine.commit()
+
+    def insert_node_setup(self, node_id, boot_count, boot_time, store_time, position="", comment=""):
+        query = f'call eco_nodes.insert_node_setup({node_id}, {boot_count}, \'{boot_time}\', \'{store_time}\', {position}, {comment});'
+        print(query)
+        self.cur.execute(query)
+        self.engine.commit()
+
+    def insert_sensor_type(self, id, type, name, manufacturer, description):
+        query = f'call eco_nodes.insert_sensor_type({id}, \'{type}\', \'{name}\', \'{manufacturer}\', \'{description}\');'
+        print(query)
+        self.cur.execute(query)
+        self.engine.commit()
+
+
 
 if __name__ == '__main__':
     from http.server import HTTPServer
