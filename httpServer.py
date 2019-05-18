fa877540cf27f0da4a172be916e68a62fcef5e73
Morten Kals <morten@kals.no>
2019-05-17 20:37:45 -0700

    fixed import for python 3

--- httpServer.py
+++ httpServer.py
@@ -1,6 +1,6 @@
 # PUSH server refference https://pymotw.com/2/BaseHTTPServer/index.html#module-BaseHTTPServer
 
-import time, threading, socket, socketserver, BaseHTTPServer
+import time, threading, socket, socketserver, http.server.HTTPServer
 import json, datetime
 import pymysql.cursors
 
