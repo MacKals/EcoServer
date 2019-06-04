53559889e04492bc064abaed7c9aa8ddd9e702da
Morten Kals <morten@kals.no>
2019-06-04 16:09:09 -0700

    test-script runs

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ db = EcoDatabase()
 #def insert_gps_point(self, node_id, boot_count, read_time, store_time, latitude, longitude, altitude, siv):
 
 db.insert_data_point(2, 59, 16, 0, '2019-06-01 13:14:35', '2019-06-01 20:15:09', 2.98)
-db.insert_node_setup()
-db.insert_node_sensor()
-db.insert_gps_point()
+#db.insert_node_setup()
+#db.insert_node_sensor()
+#db.insert_gps_point()
